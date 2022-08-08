cdd0f43c5fb894b47c204115057e016d8638e116
Bernardo A V <profesorbernardov@gmail.com>
2022-08-08 07:48:55 -0300

    signo

--- src/app/model/skills.ts
+++ src/app/model/skills.ts
@@ -2,10 +2,10 @@ export class Skills {
 
     id?: number;
     nombreS: string;
-    porcentajeS: string;
+    porcentajeS: number;
     imagenS : string;
 
-    constructor(nombreS: string, porcentajeS: string, imagenS: string) {
+    constructor(nombreS: string, porcentajeS: number, imagenS: string) {
         this.nombreS = nombreS;
         this.porcentajeS = porcentajeS;
         this.imagenS = imagenS;
